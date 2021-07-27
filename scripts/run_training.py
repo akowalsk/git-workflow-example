a8bae1a22305bd91137ebc680388532c8b3be42c
Adam Kowalski <adam@trainml.ai>
2021-07-27 13:19:28 -0500

    adding job create command

--- scripts/run_training.py
+++ scripts/run_training.py
@@ -4,11 +4,29 @@ from trainml import TrainML
 
 trainml_client = TrainML()
 
+
 async def create_job(build_serial):
     print(build_serial)
-    jobs = await trainml_client.jobs.list()
-    print(jobs)
+    job = await trainml_client.jobs.create(
+        name=f"Git Automated Training - {build_serial}",
+        type="training",
+        gpu_type="RTX 2080 Ti",
+        gpu_count=1,
+        disk_size=10,
+        worker_commands=[
+            f"git checkout {build_serial} && python main.py --epochs 10 --arch resnet50 $TRAINML_DATA_PATH 2>&1 | tee train.log"
+        ],
+        data=dict(
+            datasets=[dict(id="ImageNet", type="public")],
+            output_type="trainml",
+        ),
+        model=dict(
+            source_type="git",
+            source_uri=f"https://github.com/{os.environ.get('GITHUB_REPOSITORY')}.git",
+        ),
+    )
+    print(job)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(create_job(os.environ.get("GITHUB_SHA")))
